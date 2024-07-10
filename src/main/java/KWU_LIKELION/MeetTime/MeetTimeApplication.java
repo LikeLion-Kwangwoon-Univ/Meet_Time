38de579cb7ac979c48e0a4c94eaede8ce38a94a8
pyungjong0314 <sonopj@gmail.com>
2024-07-10 13:19:59 +0900

    First commit

---
+++ src/main/java/KWU_LIKELION/MeetTime/MeetTimeApplication.java
@@ -0,0 +1,13 @@
+package KWU_LIKELION.MeetTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KWU_LIKELION/MeetTime/MeetTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package KWU_LIKELION.MeetTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
