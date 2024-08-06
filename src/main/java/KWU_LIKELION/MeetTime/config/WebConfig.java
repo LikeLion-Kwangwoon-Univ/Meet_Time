c6d4dc91b5f1aedfeaa470db8cf459e13a42f209
limsomi <limsomi79@gmail.com>
2024-08-06 19:29:58 +0900

    config 수정

--- src/main/java/KWU_LIKELION/MeetTime/config/WebConfig.java
+++ src/main/java/KWU_LIKELION/MeetTime/config/WebConfig.java
@@ -12,7 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET","POST","PUT","DELETE","PATCH")
-                .allowedHeaders("Authorization","Content-Type")
                 .exposedHeaders("Custom-Header")
                 .allowCredentials(true)
                 .maxAge(3600);
